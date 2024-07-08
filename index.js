41f752ba5ea5ab54d9159ca4ec10239fe6fde780
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-07-08 18:59:40 +0100

    feat: integrate status command into CLI program
    Integrated the `status` command into the CLI program, allowing users to check the status of their git repository directly from the CLI.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const { program } = require("./cliSetup");
 const { initCommand } = require("./src/commands/init");
+const { statusCommand } = require("./src/commands/status");
 
 program.addCommand(initCommand);
+program.addCommand(statusCommand);
 
 program.parseAsync();
