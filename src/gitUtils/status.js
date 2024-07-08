70e7350991114535d1d161ddc43f8ff9bb11755e
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-07-08 20:05:11 +0100

    refactor: remove inline tests from `gitUtils/status.js`
    Refactored the status utility to remove inline test functions.

--- src/gitUtils/status.js
+++ src/gitUtils/status.js
@@ -5,22 +5,11 @@ const getStatus= async () => {
   return git.status();
 }
 
-// test getStatus function
-(async () => {
-  console.log(await getStatus());
-})();
-
 // check if there are changes in the repository
 const hasChanges = async () => {
   const { files } = await getStatus();
   return files.length > 0;
 }
-
-// test hasChanges function
-(async () => {
-  console.log(await hasChanges());
-}
-)();
 
 // export the functions
 module.exports = {
