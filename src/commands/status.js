711a76aa811bc6f293800b362b40d5dc578a9a0a
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-07-11 17:52:08 +0100

    feat(status): improve status command output and logic
    - Refactor the calculation of unstaged changes to correctly filter out files that have both staged and unstaged changes.
    - Change the color used for logging unstaged changes to yellow for better differentiation.

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,30 +1,34 @@
 const { getStatus, hasChanges } = require("../gitUtils/status");
 const { Command } = require("commander");
-const { red, green } = require("colorette");
+const { red, green, yellow } = require("colorette");
 
 const log = console.log;
 
 async function status() {
-  const { not_added, created, deleted, modified, renamed, files, staged } =
+  const { not_added, created, deleted, modified, renamed, staged, files } =
     await getStatus();
 
   const untracked = {
-    message: "untracked files: ",
+    message: "Untracked files:",
     files: [...not_added],
   };
 
-  const unStaged = [
+  const unstagedChanges = [
     ...new Set([...created, ...deleted, ...modified, ...renamed]),
-  ];
+  ].filter(
+    (file) =>
+      !staged.includes(file) ||
+      files.some((f) => f.path === file && f.working_dir !== " "),
+  ); // Include files that have both staged and unstaged changes
 
   const readyToBeCommitted = {
     message: "Changes to be committed:",
-    files: [...new Set([...staged])],
+    files: [...staged],
   };
 
   const unStagedObj = {
     message: "Changes not staged for commit:",
-    files: [...unStaged],
+    files: [...unstagedChanges],
   };
 
   return {
@@ -38,35 +42,34 @@ const program = new Command();
 const statusCommand = program
   .command("status")
   .description(
-    "List changes to be committed and changes not staged for commit, and untracked files",
+    "List changes to be committed, changes not staged for commit, and untracked files",
   )
   .action(async () => {
-  const { untracked, readyToBeCommitted, unStagedObj } = await status();
+    const { untracked, readyToBeCommitted, unStagedObj } = await status();
 
-  const changes=await hasChanges();
-  if(!changes) {
-    log(green("nothing to commit, working tree clean"));
-    log(await getStatus());
-    return;
-  }
+    const changes = await hasChanges();
+    if (!changes) {
+      log(green("nothing to commit, working tree clean"));
+      log(await getStatus());
+      return;
+    }
 
-  const logList = (message, files, color) => {
-    log(message);
-    files.forEach((file, index) => log(color(`${index + 1}. ${file}`)));
-  };
+    const logList = (message, files, color) => {
+      log(message);
+      files.forEach((file, index) => log(color(`${index + 1}. ${file}`)));
+    };
 
-  if (untracked.files.length > 0) {
-    logList(untracked.message, untracked.files, red);
-  }
+    if (untracked.files.length > 0) {
+      logList(untracked.message, untracked.files, red);
+    }
 
-  if (readyToBeCommitted.files.length > 0) {
-    logList(readyToBeCommitted.message, readyToBeCommitted.files, green);
-  }
+    if (readyToBeCommitted.files.length > 0) {
+      logList(readyToBeCommitted.message, readyToBeCommitted.files, green);
+    }
 
-  if (unStagedObj.files.length > 0) {
-    logList(unStagedObj.message, unStagedObj.files, red);
-  }
-});
+    if (unStagedObj.files.length > 0) {
+      logList(unStagedObj.message, unStagedObj.files, yellow);
+    }
+  });
 
 module.exports = { statusCommand };
-
